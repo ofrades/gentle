d4aaa9e4dd17dbde44fc52e7086a9e71659a7242
miguel/ofrades <mig.silva@gmail.com>
2021-01-25 09:59:01 +0000

    feat: add react-query and getServerProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
-import styled from 'styled-components'
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+const queryClient = new QueryClient();
+import Tweets from './../components/Tweets/index';
+import { getTweets } from './../services/tweet';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle, { theme } from './../styles/globalStyles.js';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
-
-export default function Home() {
-  return <Title>My page</Title>
+export default function Home({ tweets }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <Tweets tweets={tweets} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+}
+export async function getServerProps() {
+  const tweets = await getTweets();
+  return { props: { tweets } };
 }
